d50148d2ff2e2a5bb4626908e1ed70480179b690
Yiğit İrez <yigit.irez@softtech.com.tr>
2018-02-07 12:15:18 +0300

    Add project files.

---
+++ TextCorrupter/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TextCorrupter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOriginal = new System.Windows.Forms.TextBox();
+            this.txtModified = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkIsFromRunic = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOriginal
+            // 
+            this.txtOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOriginal.Location = new System.Drawing.Point(3, 16);
+            this.txtOriginal.Multiline = true;
+            this.txtOriginal.Name = "txtOriginal";
+            this.txtOriginal.Size = new System.Drawing.Size(565, 105);
+            this.txtOriginal.TabIndex = 0;
+            // 
+            // txtModified
+            // 
+            this.txtModified.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtModified.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtModified.Location = new System.Drawing.Point(3, 16);
+            this.txtModified.Multiline = true;
+            this.txtModified.Name = "txtModified";
+            this.txtModified.Size = new System.Drawing.Size(565, 110);
+            this.txtModified.TabIndex = 1;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnTranslate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTranslate.Location = new System.Drawing.Point(571, 0);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 282);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtOriginal);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 124);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Original:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtModified);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 153);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(571, 129);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modified:";
+            // 
+            // chkIsFromRunic
+            // 
+            this.chkIsFromRunic.AutoSize = true;
+            this.chkIsFromRunic.Dock = System.Windows.Forms.DockStyle.Right;
+            this.chkIsFromRunic.Location = new System.Drawing.Point(472, 124);
+            this.chkIsFromRunic.Name = "chkIsFromRunic";
+            this.chkIsFromRunic.Size = new System.Drawing.Size(99, 29);
+            this.chkIsFromRunic.TabIndex = 5;
+            this.chkIsFromRunic.Text = "from Runic(-ish)";
+            this.chkIsFromRunic.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 282);
+            this.Controls.Add(this.chkIsFromRunic);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnTranslate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Translator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOriginal;
+        private System.Windows.Forms.TextBox txtModified;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkIsFromRunic;
+    }
+}
+
---
+++ TextCorrupter/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO; 
+using System.Text; 
+using System.Windows.Forms;
+
+namespace TextCorrupter
+{
+    public partial class Form1 : Form
+    {
+        private String translatorFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "translatorLicence"); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            if (checkUsability())
+            {
+                txtModified.Text = SpecialCharToModified(txtOriginal.Text, chkIsFromRunic.Checked);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(txtOriginal.Text) && txtOriginal.Text.Length > 5)
+                {
+                    MessageBox.Show(null, "This demo version can only convert 5 characters at a time. Obtain a license and place in: \"" + translatorFolder + "\" folder. :)", "No.", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                else
+                    txtModified.Text = SpecialCharToModified(txtOriginal.Text, chkIsFromRunic.Checked);
+            }
+        }
+
+        private Boolean checkUsability()
+        {  
+            if (!Directory.Exists(translatorFolder))
+                Directory.CreateDirectory(translatorFolder);
+
+            string licenceFilePath=translatorFolder + @"\lic.txt";
+
+            if (!File.Exists(licenceFilePath))
+            {
+                File.Create(licenceFilePath).Close();
+            }
+
+            using (StreamReader sr = new StreamReader(licenceFilePath, true))
+            {
+                string result = sr.ReadToEnd();
+                if (result.Contains("this is a licence"))
+                    return true;
+            }  
+            return false;
+        }
+
+        private static string SpecialCharToModified(string inString, Boolean decode)
+        {
+
+            String[] olds = { "T", "B", "S", "D", "H", "G", "A", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "y", "z", "ç", "ı", "ş", "ü", "ğ" };
+            String[] news = { "ᛪ", "ᛟ", "ᛠ", "ᛢ", "ᛰ", "ᛯ", "ᛮ", "ᚠ", "ᚡ", "ᚢ", "ᚣ", "ᚤ", "ᚥ", "ᚦ", "ᚧ", "ᛥ", "ᚩ", "ᛤ", "ᚫ", "ᚰ", "ᚱ", "ᚲ", "ᚳ", "ᚴ", "ᚵ", "ᚶ", "ᚷ", "ᚸ", "ᚹ", "ᚻ", "ᚺ", "ᚻ", "ᛄ", "ᛉ", "ᛋ", "ᛁ" };
+
+            for (int i = 0; i < olds.Length; i++)
+            {
+                if (!decode)
+                    inString = inString.Replace(olds[i], news[i]);
+                else
+                    inString = inString.Replace(news[i], olds[i]);
+            }
+
+            return inString; 
+        }   
+    }
+}
